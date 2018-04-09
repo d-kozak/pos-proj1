580e0fa0253cb80265b04775e4b6d0aaf37253b2
dkozak <dkozak94@gmail.com>
2018-04-09 19:38:37 +0200

    simple example

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
+#define _XOPEN_SOURCE
+#define _XOPEN_SOURCE_EXTENDED 1 /* XPG 4.2 - needed for WCOREDUMP() */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 
 int main(int arc, char **argv) {
-    puts("BOOOO!");
+    if(arc == 1){
+        printf("%s",argv[0]);
+    }
 
-    if(arc < 1){
-        printf("%s",argv[1]);
+
+    int status;
+    pid_t id = fork();
+    if (id == -1) {
+        perror("fork");
+        return 1;
+    } else if (id != 0) {
+        // parent
+        signal(SIGINT, SIG_IGN);
+        waitpid(id, &status, 0);
+        if (WIFEXITED(status)) {
+            printf("child %d exited with %d\n", (int) id, WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("child %d signaled with %d\n", (int) id, WTERMSIG(status));
+        }
+        return 0;
+    } else {
+        // child
+        sleep(10);
+        return 1;
     }
 }
