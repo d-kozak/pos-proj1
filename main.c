981b0936bf034f87265945de57b88e39f4285394
dkozak <dkozak94@gmail.com>
2018-04-09 21:15:58 +0200

    finish the main part of the project

--- main.c
+++ main.c
@@ -7,30 +7,66 @@
 #include <unistd.h>
 
 
-int main(int arc, char **argv) {
-    if(arc == 1){
-        printf("%s",argv[0]);
+void printStartInfo(char *label) {
+    printf("%s identification: \n", label); /*grandparent/parent/child */
+    printf("	pid = %d,	ppid = %d,	pgrp = %d\n", getpid(), getppid(), getpgrp());
+    printf("	uid = %d,	gid = %d\n", getuid(), getgid());
+    printf("	euid = %d,	egid = %d\n", geteuid(), getegid());
+}
+
+void printFinishInfo(char *label, pid_t id, int status) {
+    printf("%s exit (pid = %d):", label, id);
+    if (WIFEXITED(status)) {
+        printf("	normal termination (exit code = %d)\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+#ifdef WCOREDUMP
+        printf("	signal termination %s(signal = %d)\n", WCOREDUMP(status) ? "with core dump" : "",
+               WTERMSIG(status));
+#else
+        printf("	signal termination %s(signal = %d)\n", "", WTERMSIG(status));
+#endif
+    } else {
+        printf("	unknown type of termination\n");
     }
+}
 
-
+int main(int arc, char **argv) {
+    if (arc < 2) {
+        fprintf(stderr, "At least one paremeter needed - program to execute in the child process\n");
+        return 1;
+    }
+    printStartInfo("grandparent");
     int status;
     pid_t id = fork();
     if (id == -1) {
         perror("fork");
         return 1;
     } else if (id != 0) {
-        // parent
+        // grandparent
         signal(SIGINT, SIG_IGN);
         waitpid(id, &status, 0);
-        if (WIFEXITED(status)) {
-            printf("child %d exited with %d\n", (int) id, WEXITSTATUS(status));
-        } else if (WIFSIGNALED(status)) {
-            printf("child %d signaled with %d\n", (int) id, WTERMSIG(status));
-        }
+        printFinishInfo("parent", id, status);
         return 0;
     } else {
-        // child
-        sleep(10);
-        return 1;
+        printStartInfo("parent");
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return 1;
+        } else if (pid != 0) {
+            signal(SIGINT, SIG_IGN);
+            waitpid(id,
+                    &status, 0);
+            printFinishInfo("child", pid, status
+            );
+            return 0;
+        } else {
+            printStartInfo("child");
+            if(execv(argv[1], argv + 1) != 0){
+                perror("execv");
+                return 1;
+            }
+            return 0;
+        };
     }
 }
